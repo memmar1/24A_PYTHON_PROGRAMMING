6db9de1b51ebb2e21066d5e4b501a590de7bde8d
Wilberforce Wairagu Wahome <willy.wairagu@gmail.com>
2024-01-09 20:59:39 +0300

    my first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Welcome to Zindua School Python Programming Class
+
+### Basic Scripting Commands
+1. cd - change directory - helps you navigate between folders
+2. ls - list files or contents of a folder;
+3. pwd - shows path to your current working location
+
+### Basic Git Commands
+1. git init - Helps you initialize an Empty repository
+2. git branch -M main - navigating to the main branch 
+3. git remote add origin path - creating a connaction to our remote repository
+3. git remote -v - Checking the remote repo we are working off from
---
+++ week_1/Introduction.py
@@ -0,0 +1,75 @@
+# Output: Hello World!
+print("Hello World!")
+
+# This is a single line comment
+"""
+ This is a multiline comment 
+ Welcome to python Programming 
+"""
+
+#  Variables - defined using a =
+name = "Mercy"
+a = 10 
+
+print(a)
+
+# Data Types
+# 1. String - sequence of characters (Think of strings as text)
+string_1 = "This is a string"
+print("my first string: ", string_1)
+
+# 2. Integer - whole numbers
+integer_1 = 5
+
+print("my first integer: ", integer_1)
+
+# 3. Float - decimal numbers
+float_1 = 5.5
+
+print("my first float: ", float_1)
+
+# 4. Boolean - True or False
+state_1 = True
+
+print("my first boolean: ", state_1)
+
+# How to check the data type of a variable
+print(type(state_1))
+
+a = 1 # Assigning a value to a variable
+b = 2
+
+print(a == 2) # Comparing a variable to a value
+
+# Arithmetic Operators
+# + - addition
+print(a + b)
+
+# - - subtraction
+print(a - float_1)
+
+# * - multiplication
+print(float_1 * b)
+
+# / - division
+print(a / b)
+
+# % - modulus - This gives the remainder of a division
+print(7 % 2)
+
+# % - floor division - This gives the quotient of a division 
+print(7 // 2)
+
+# ** - exponent
+print(2 ** 3)
+
+# Comparison Operators
+print(a == 1) # Comparing a variable to a value
+
+# and - returns True if both conditions are True
+print(a == 1 and b == 3)
+
+# or - returns True if one of the conditions is True
+print(a == 2 or b == 3)
+
+
